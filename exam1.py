c5433f714db2f7c54fee11ceba9799f06cddcfba
Park_Notebook <whddn859@naver.com>
2023-03-31 14:06:38 +0900

    feat: create exam1, 2

---
+++ exam1.py
@@ -0,0 +1,6 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120804
+def solution(num1, num2):
+    answer = 0
+    answer = 'amsksdmsdkskd'
+    answer = num1 * num2
+    return answer
---
+++ exam2.py
@@ -0,0 +1,6 @@
+#https://school.programmers.co.kr/learn/courses/30/lessons/120803
+
+def solution(num1, num2):
+    answer = 0
+    answer = num1 - num2
+    return answer
